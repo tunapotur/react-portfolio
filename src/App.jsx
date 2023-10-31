135dd70aeb0b1b361663451f2b9f02ce47d14f62
Ahmet Tuna POTUR <tpotur@gmail.com>
2023-10-31 22:04:41 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+function App() {
+  return (
+    <div className="mx-auto flex min-h-screen w-[64rem] ">
+      <nav className="w-[16rem] bg-slate-200"></nav>
+      <main className="w-[48rem] border border-slate-500"></main>
+    </div>
+  );
+}
+
+export default App;
+
+/*
+1rem 16px
+x    1024px
+
+1024/16 = 64rem
+48*16 = 768
+
+
+*/
