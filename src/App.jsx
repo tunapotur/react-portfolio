0d0349198e9a5ed9a7d685bd239e2ae50f69fc5e
Ahmet Tuna POTUR <tpotur@gmail.com>
2023-11-02 21:40:41 +0300

    navbar first design

--- src/App.jsx
+++ src/App.jsx
@@ -3,38 +3,49 @@ import { BsDownload } from 'react-icons/bs';
 function App() {
   //TODO when project finish specify heropattern for reducing css size
   return (
-    <div className="relative mx-auto flex min-h-screen w-[64rem] ">
-      <nav className="flex w-[16rem] flex-col items-center bg-slate-200">
+    <div className="mx-auto flex h-screen w-[64rem] flex-row py-5">
+      <nav className="flex h-full max-h-screen w-1/4 flex-grow flex-col items-center overflow-y-auto rounded-l-md border-y border-l border-slate-800 bg-slate-200 px-5 pb-1 pt-5 text-center">
         <img
           src="../navbar-user-image.jpg"
           alt="user navbar photo"
           className="mt-10 h-36 rounded-full"
         />
-        <h3>Ahmet Tuna Potur</h3>
-        <h2>ICT Teacher &amp; Engineer</h2>
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Education</li>
-          <li>Work Experience</li>
-          <li>My Sample Projects</li>
-          <li>Foreign Language &amp; Skills</li>
-          <li>Achievements</li>
-          <li>Certificates</li>
-          <li>Udemy Certificates</li>
-          <li>References</li>
-          <li>Personal Information &amp; Contact</li>
+        <h3 className="font-nunito mb-2 mt-3 text-3xl font-semibold leading-7">
+          Ahmet Tuna Potur
+        </h3>
+        <h2 className="font-open_sans mb-10 font-medium">
+          ICT Teacher &amp; Engineer
+        </h2>
+
+        <ul className="leading-5">
+          <li className="mb-4 text-lg font-semibold leading-5 text-amber-700">
+            Home
+          </li>
+          <li className="mb-4">About</li>
+          <li className="mb-4">Education</li>
+          <li className="mb-4">Work Experience</li>
+          <li className="mb-4">My Sample Projects</li>
+          <li className="mb-4">Foreign Language &amp; Skills</li>
+          <li className="mb-4">Achievements</li>
+          <li className="mb-4">Certificates</li>
+          <li className="mb-4">Udemy Certificates</li>
+          <li className="mb-4">References</li>
+          <li className="mb-4">Personal Information &amp; Contact</li>
         </ul>
 
-        <div className="flex items-center border border-slate-800 px-3 py-2">
-          <span className="pr-2">Download CV</span>
-          <BsDownload />
-        </div>
+        <div className="mb-2 mt-auto text-xs text-slate-600">
+          <div className="mb-2 flex items-center border-[1px] border-slate-400 px-3 py-2">
+            <span className="pr-2">Download CV</span>
+            <BsDownload />
+          </div>
 
-        <p>Ahmet Tuna Potur</p>
-        <p>tunapotur@yahoo.com</p>
+          <div className="italic leading-3">
+            <p>Ahmet Tuna Potur</p>
+            <p>tunapotur@yahoo.com</p>
+          </div>
+        </div>
       </nav>
-      <main className="w-[48rem] bg-repeat heropattern-circuitboard-slate-100/90"></main>
+      <main className="max-h-screen w-3/4 overflow-y-auto rounded-r-md border border-slate-800 bg-repeat heropattern-circuitboard-slate-200/90"></main>
     </div>
   );
 }
@@ -43,4 +54,8 @@ export default App;
 
 /**
  * 1rem=16px
+ *
+ * font-family: 'Montserrat', sans-serif;
+ * font-family: 'Nunito Sans', sans-serif;
+ * font-family: 'Open Sans', sans-serif;
  */
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['Montserrat', 'sans-serif'],
+    },
+    extend: {
+      fontFamily: {
+        nunito: ['"Nunito Sans"', 'sans-serif'],
+        open_sans: ['"Open Sans"', 'sans-serif'],
+      },
+    },
   },
   plugins: [require('tailwindcss-hero-patterns')],
 };
+
+/**
+ * 
+ *     fontFamily: {
+      sans: 'Roboto Mono, monospace',
+    },
+
+     * font-family: 'Montserrat', sans-serif;
+ * font-family: 'Nunito Sans', sans-serif;
+ * font-family: 'Open Sans', sans-serif;
+ */
