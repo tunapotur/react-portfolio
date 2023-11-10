2b95ebcd769239f2e42bd6cb651800cea18ce8a9
Ahmet Tuna POTUR <tpotur@gmail.com>
2023-11-10 20:09:34 +0300

    rootList added

---
+++ src/data/rootList.js
@@ -0,0 +1,19 @@
+const rootList = [
+  { id: 1, path: 'home', text: 'Home' },
+  { id: 2, path: 'about', text: 'About' },
+  { id: 3, path: 'education', text: 'Education' },
+  { id: 4, path: 'workexperience', text: 'Work Experience' },
+  { id: 5, path: 'mysampleprojects', text: 'My Sample Projects' },
+  { id: 6, path: 'languageandskills', text: 'Foreign Language &amp; Skills' },
+  { id: 7, path: 'achievements', text: 'Achievements' },
+  { id: 8, path: 'certificates', text: 'Certificates' },
+  { id: 9, path: 'udemycertificates', text: 'Udemy Certificates' },
+  { id: 10, path: 'references', text: 'References' },
+  {
+    id: 11,
+    path: 'personelinfoandcontact',
+    text: 'Personal Information &amp; Contact',
+  },
+];
+
+export default rootList;
