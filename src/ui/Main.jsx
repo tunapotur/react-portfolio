432ceb3ffbd02b3326819550c6baee905725c6f2
Ahmet Tuna POTUR <tpotur@gmail.com>
2023-11-15 12:16:39 +0300

    main fixed

--- src/ui/Main.jsx
+++ src/ui/Main.jsx
@@ -5,8 +5,8 @@ function Main({ children }) {
 
   return (
     <main
-      className={`translate-animation flex flex-col items-center overflow-y-auto px-[3rem] pb-10 pt-24 ${
-        isSidebarOpen ? 'ml-[16rem] w-[48rem]' : 'mx-[8rem] w-[48rem]'
+      className={`translate-animation flex w-[48rem] flex-col items-center overflow-y-auto px-[3rem] pb-10 pt-24 ${
+        isSidebarOpen ? 'ml-[16rem]' : 'mx-[8rem]'
       }`}
     >
       {children}
