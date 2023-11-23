bb1ad1485c15239596089e281ece3499ffd42cab
Ahmet Tuna POTUR <tpotur@gmail.com>
2023-11-23 12:02:05 +0300

    LanguageAndSkills is fixed

--- src/pages/LanguageAndSkills.jsx
+++ src/pages/LanguageAndSkills.jsx
@@ -4,12 +4,8 @@ import axios from 'axios';
 import PaginationAuto from '../ui/PaginationAuto';
 import ShowDataContent from '../ui/ShowDataContent';
 
-import useGetData from '../hooks/useGetData';
-import PageUi from '../ui/PageUi';
-
 import { BsCheckLg } from 'react-icons/bs';
 
-// TODO work experience alanı önceden tek veriden çalışıyordu şimdi 3 farklı veriyi alıp yüklemek gerek
 function LanguageAndSkills() {
   const [skills, language] = useQueries({
     queries: [
@@ -29,53 +25,51 @@ function LanguageAndSkills() {
       },
     ],
   });
-  return <div className="content-narrow"></div>;
-}
-
-/*
-  <PageUi
-      isPending={isPending}
-      isError={isError}
-      error={error}
-      pageHeader={'Foreign Languages & Skills'}
-    >
-      <LanguageAndSkillsUi data={data} />
-    </PageUi>
-  
-*/
-
-function LanguageAndSkillsUi({ data }) {
   return (
     <div className="content-narrow">
-      <LanguageOrSkillsConten
-        data={data.foreignLanguage}
-        header={'Foreign Languages'}
-      />
+      <h1 className="@apply mb-6 text-xl font-semibold sm:mb-10 sm:text-2xl">
+        Work Experience
+      </h1>
 
-      <LanguageOrSkillsConten data={data.skills} header={'Skills'} />
+      <div className="mb-12 flex flex-col justify-center space-y-4">
+        <h2 className="text-xl font-semibold">Language</h2>
+        <ul className="readable-background border-color flex flex-col space-y-3 border p-4">
+          <ShowDataContent
+            data={language}
+            fnRender={(item) => <LanguageSkillCart key={item.id} data={item} />}
+            fnFilter={(item) => item.language === 'en'}
+          />
+        </ul>
+      </div>
+
+      <div className="mb-12 flex flex-col justify-center space-y-4">
+        <h2 className="text-xl font-semibold">Skills</h2>
+        <ul className="readable-background border-color flex flex-col space-y-3 border p-4">
+          <ShowDataContent
+            data={skills}
+            fnRender={(item) => <LanguageSkillCart key={item.id} data={item} />}
+            fnFilter={(item) => item.language === 'en'}
+          />
+        </ul>
+      </div>
+
+      <PaginationAuto />
     </div>
   );
 }
 
-function LanguageOrSkillsConten({ data, header }) {
+function LanguageSkillCart({ data }) {
   return (
-    <div className="mb-12 flex flex-col justify-center space-y-4">
-      <h2 className="text-xl font-semibold">{header}</h2>
-      <ul className="readable-background border-color flex flex-col space-y-3 border p-4">
-        {data.map((el) => (
-          <li
-            className="flex flex-row items-center text-left leading-tight"
-            key={el.id}
-          >
-            <div className="min-w-5 min-h-5 mr-1">
-              <BsCheckLg />
-            </div>
-            <div>{el.language ? el.language : el.skill}</div>
-            {el.level && <span className="ml-auto">{el.level}</span>}
-          </li>
-        ))}
-      </ul>
-    </div>
+    <li
+      className="flex flex-row items-center text-left leading-tight"
+      key={data.id}
+    >
+      <div className="min-w-5 min-h-5 mr-1">
+        <BsCheckLg />
+      </div>
+      <div>{data.explanation}</div>
+      {data.level && <span className="ml-auto">{data.level}</span>}
+    </li>
   );
 }
 
